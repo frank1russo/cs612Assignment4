fd7d4bfa5052c16c63b1c68bf2d3f08856dc7513
Frank Russo <fr50558n@pace.edu>
2018-06-27 10:36:22 -0400

    Added support for html content in the blogs.json.

--- WebContent/BlogEntry.js
+++ WebContent/BlogEntry.js
@@ -10,7 +10,8 @@ class BlogEntry extends React.Component {
   render() {
 	  const title = e('div', null, e('h1', null, `${this.props.title}`));
 	  const date = e('div', null, e('h3', null, `${this.props.date}`));
-	  const body = e('div', null, `${this.props.body}`);
+//	  const body = e('div', null, `${this.props.body}`);
+	  const body = e('div', {dangerouslySetInnerHTML: {__html: this.props.body}}, null);
 	  return e('div', null, [title, date, body]);
   }
 }
